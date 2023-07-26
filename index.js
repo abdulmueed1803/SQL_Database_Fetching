bea21902b6ebb017cc29df1eb822a915137bcaa9
abdulmueed1803 <abdul.mueed20@vit.edu>
2023-07-26 15:14:37 +0530

    Added time constraint

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const { Pool } = require('pg');
 const bodyParser = require('body-parser');
 const dotenv = require("dotenv");
 const ejs = require('ejs') ;
+const PDFDocument = require('pdfkit');
+const blobStream = require('blob-stream');
 
 const app = express();
 const port = 3000;
@@ -28,31 +30,53 @@ app.get("/", (req, res) => {
 
 app.post("/", async(req, res) => {
     
-    const {sdate, edate} = req.body ;
-    console.log(`${sdate} ${edate}`);
-    console.log(typeof(sdate));
+    const sdate = req.body.sdate ;
+    const edate = req.body.edate ;
+   
     
+    let stime =parseInt(req.body.stime.substring(0,2));
+    let etime = parseInt(req.body.etime.substring(0,2)) ;
+    console.log(`${stime} ${etime}`);
+    console.log(typeof(stime)) ;
+
+
     const query = {
         name: "fetch-database",
-        text: `SELECT payment_id, amount, TO_CHAR(payment_date,'DD-MM-YYYY')
-            FROM payment WHERE DATE(payment_date)
-            BETWEEN $1 AND $2
-            ORDER BY DATE(payment_date)`,
-        values: [sdate,edate]
+        text: `SELECT payment_id, amount, 
+            TO_CHAR(payment_date,'DD-MM-YYYY') AS DATE, 
+            TO_CHAR(payment_date, 'HH12:MI:SS') AS TIME
+            FROM payment 
+            WHERE DATE(payment_date) BETWEEN $1 AND $2
+            AND EXTRACT(HOUR FROM payment_date) BETWEEN $3 AND $4
+            ORDER BY payment_date`,
+        values: [sdate,edate, stime, etime]
         
     }
 
     const result = await pool.query(query);
-    console.log(result.rows);
+    console.log(result.rows[0]);
     res.render("sql",{
-        entries: result.rows
+        entries: result.rows,
+        sdate: sdate,
+        edate: edate
     });
     
 });
  
+app.post('/download-pdf', async(req, res) => {
+   
+try {
+       
+  
+ } catch (error) {
+ 
+ } 
+        
+
+});
  
     
   
   app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
-  });
+});
